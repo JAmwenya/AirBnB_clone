d5e8f39b120eb9f315f50958e0f3b01721cbc454
root <bolodenis>
2024-01-11 07:32:04 -0800

    adding modules

---
+++ models/city.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+""" a module  that defines a city"""
+class City(BaseModel):
+    """ class that creates a city"""
+    state_id:str = " "
+    name:str = " "
