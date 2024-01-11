c9b7ac08e91cafc3497a9a96c2205d56ada91cfc
JAmwenya <jnehondo@gmail.com>
2024-01-11 15:40:37 +0000

    models

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+class that defines an amenity
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """class that creates an amenity"""
+    name = ""
