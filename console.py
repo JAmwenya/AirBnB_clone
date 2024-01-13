5e5678a7212ea1c0ca563b1c8324c009a0a09324
root <bolodenis>
2024-01-13 05:00:14 -0800

    adding changes

--- console.py
+++ console.py
@@ -7,10 +7,10 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb)"
     def do_quit(self, line):
-        """ Quit command to exit program"""
+        """ Quit command to exit the program"""
         return True
     def do_EOF(self, line):
-        """ Quit command to exit program"""
+        """ Quit command to exit the program"""
         return True
     def do_empty(self, line):
         """
