e8f66885c119321c98252bc93f97553b6fe80464
root <bolodenis>
2024-01-13 05:04:35 -0800

    tracking changes

--- console.py
+++ console.py
@@ -5,7 +5,7 @@ class HBNBCommand(cmd.Cmd):
     entry point of the command interpreter
 
     """
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
     def do_quit(self, line):
         """ Quit command to exit the program"""
         return True
